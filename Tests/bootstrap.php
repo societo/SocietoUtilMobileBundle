80bc7f76dba6b009fe69d2b08ce249f477833bbe
Kousuke Ebihara <ebihara@php.net>
2011-09-11 06:55:15 +0900

    changed bootstrap to set SYMFONY__KERNEL__ROOT_DIR to tell a valid kernel path

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
 
-require_once (isset($_SERVER['KERNEL_DIR']) ? $_SERVER['KERNEL_DIR'] : __DIR__.'/../../../../../app').'/bootstrap.php.cache';
+if (!isset($_SERVER['KERNEL_DIR'])) {
+    $_SERVER['KERNEL_DIR'] = __DIR__.'/../../../../../app';
+}
+
+$_SERVER['SYMFONY__KERNEL__ROOT_DIR'] = $_SERVER['KERNEL_DIR'];
+
+require_once $_SERVER['KERNEL_DIR'].'/bootstrap.php.cache';
